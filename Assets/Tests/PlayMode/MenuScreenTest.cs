1afd463dfedbf4be0502ca573c5a4c4624d16e49
ketcode <kesse.matias@hotmail.com>
2024-10-24 15:00:47 -0300

    fix: add comments and changed testSceneLoading

--- Assets/Tests/PlayMode/MenuScreenTest.cs
+++ Assets/Tests/PlayMode/MenuScreenTest.cs
@@ -18,30 +18,28 @@ public class MenuScreenTest
     [Order(1)]
     public IEnumerator TestSceneLoading()
     {
-        // --------- LOAD SCENE --------- //
-        var testScene = SceneManager.GetActiveScene();
-        yield return SceneManager.LoadSceneAsync("Menu", LoadSceneMode.Additive);
-        SceneManager.SetActiveScene(SceneManager.GetSceneByName("Menu"));
-        
+        // --------- SCENE NAME CHECK --------- //
         Assert.IsTrue(SceneManager.GetActiveScene().name == "Menu");
         
-        // --------- CLEAN UP --------- //
-        SceneManager.SetActiveScene(testScene);
-        yield return SceneManager.UnloadSceneAsync("Menu");
+        yield return new WaitForFixedUpdate();
     }
 
     [UnityTest]
     public IEnumerator PlayGame_GameStart_Success()
     {
+        // --------- SCENE NAME CHECK --------- //
         string sceneName = SceneManager.GetActiveScene().name;
         Assert.That(sceneName, Is.EqualTo("Menu"));
         
+        // --------- PLAY BUTTON CHECK --------- //
         Button playButton = GameObject.Find("Background/Button Container/PlayButton").GetComponent<Button>();
         Assert.IsNotNull(playButton, "'PlayButton' GameObject not found in scene.");
         
+        // --------- CLICK BUTTON --------- //
         new Interaction().Click().Button(playButton).Perform();
         yield return new WaitForDomainReload();
         
+        // --------- CHANGE SCENE CHECK --------- //
         sceneName = SceneManager.GetActiveScene().name;
         Assert.That(sceneName, Is.EqualTo("Game"));
     }
@@ -49,15 +47,19 @@ public class MenuScreenTest
     [UnityTest]
     public IEnumerator ExitGame_GameClose_Success()
     {
+        // --------- SCENE NAME CHECK --------- //
         string sceneName = SceneManager.GetActiveScene().name;
         Assert.That(sceneName, Is.EqualTo("Menu"));
         
+        // --------- QUIT BUTTON CHECK --------- //
         Button quitButton = GameObject.Find("Background/Button Container/QuitButton").GetComponent<Button>();
         Assert.IsNotNull(quitButton, "'PlayButton' GameObject not found in scene.");
         
+        // --------- CLICK BUTTON --------- //
         new Interaction().Click().Button(quitButton).Perform();
         yield return new WaitForDomainReload();
         
+        // --------- EXIT GAME --------- //
         // Application.Quit() não encerra o editor durante os testes, então não é possível verificar se a aplicação realmente fecha.
         Assert.IsTrue(Application.isPlaying, "The application must be running after clicking Exit.");
     }
